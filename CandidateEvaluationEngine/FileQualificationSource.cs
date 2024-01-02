77449c0ec4b6e4e9cee31397bdeae525f991a01a
mohammadamer <m.amer@live.com>
2024-01-02 20:39:39 +0000

    Applied Single Responsibility Principle

---
+++ CandidateEvaluationEngine/FileQualificationSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateEvaluationEngine
+{
+    public class FileQualificationSource
+    {
+        public string GetQualificationFromSource()
+        {
+            return File.ReadAllText("Qualification.json");
+        }
+    }
+}
---
+++ CandidateEvaluationEngine/JsonQualificationSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CandidateEvaluationEngine
+{
+    public class JsonQualificationSerializer
+    {
+        public Qualification GetQualificationFromJsonString(string jsonString)
+        {
+            return JsonSerializer.Deserialize<Qualification>(jsonString, new JsonSerializerOptions
+            {
+                Converters = { new JsonStringEnumConverter() },
+                PropertyNameCaseInsensitive = true // If you want case-insensitive property matching
+            });
+        }
+    }
+}
